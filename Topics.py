7bbe88b2331bf0811f8dfc1db453400cfdf9399e
nathantfrank <nathan.t.frank@gmail.com>
2016-10-27 19:35:28 -0500

    "Initial Topic Classes and Visual Modeler Class"

---
+++ Topics.py
@@ -0,0 +1,57 @@
+'''
+#####################################################################################################
+#	CSE 4214, Intro to Software Engineering, Fall 2016
+#	Lab Section 2, Group 3, Next Top Model
+#
+#####################################################################################################
+#	Contributors:
+#			Alex Palacio, Christopher Cole, Jia Zhao,
+#			Nathan Frank, Reid Montague, Titus Dillon
+#
+#####################################################################################################
+#	Program info:
+#
+#
+#
+#
+#####################################################################################################
+#   TODO:
+#       Fill in functions
+#
+#
+#
+#####################################################################################################
+#!/usr/bin/python
+'''
+
+
+class EnhancementTopic:
+
+    def __init__(self):
+        self.__wordList = []
+        self.__dateCount = {}
+
+    def setWords(self, wordList):
+        return None
+
+    def incDateCount(self, date):
+        return None
+
+    def getDateCount(self, date):
+        return None
+
+
+class BugTopic:
+
+    def __init__(self):
+        self.__bugMap = {}
+        self.__severityMap = {}
+
+    def incDateCount(self, date, severity):
+        return None
+
+    def getDateCount(self, date, severity):
+        return None
+
+    def setWords(self, wordList, severity):
+        return None
---
+++ VisualModeler.py
@@ -0,0 +1,49 @@
+'''
+#####################################################################################################
+#	CSE 4214, Intro to Software Engineering, Fall 2016
+#	Lab Section 2, Group 3, Next Top Model
+#
+#####################################################################################################
+#	Contributors:
+#			Alex Palacio, Christopher Cole, Jia Zhao,
+#			Nathan Frank, Reid Montague, Titus Dillon
+#
+#####################################################################################################
+#	Program info:
+#
+#
+#
+#
+#####################################################################################################
+#   TODO:
+#       Fill in functions
+#
+#
+#
+#####################################################################################################
+#!/usr/bin/python
+'''
+
+from ggplot import *
+
+
+class VisualModeler:
+
+    def __init__(self):
+        # just a placeholder atm
+        self.__name = "Visual Modeling Class"
+
+    def modeVolumeEnhView(self, enhTopics):
+        return None
+
+    def modeDateView(self, enhTopic):
+        return None
+
+    def modelVolumeBugView(self, bugTopics):
+        return None
+
+    def modelMultiDateView(self, bugTopic):
+        return None
+
+    def modelDividedView(self, bugTopic):
+        return None
