8cb2fa30c7f88203fea734a8d6d349958dba729b
cc1533 <cc1533@msstate.edu>
2016-11-03 20:29:52 -0500

    Updated the Layout issues

--- qtgui.py
+++ qtgui.py
@@ -7,31 +7,42 @@ class Form(QWidget):
         
         inputlabel = QLabel("Input File Location:")
         self.inputline = QLineEdit("/Path/To/Input/File.xlsx")
+        self.inputSearch = QPushButton("...")
         self.parse = QPushButton("Parse Input")
  
         malletlabel = QLabel("Mallet Location:")
         self.malletline = QLineEdit("/Path/To/Mallet")
+        self.malletSearch = QPushButton("...")
         self.callmallet = QPushButton("Call Mallet")
         
         self.enhCheck = QCheckBox("View Enhancements")
         self.bugCheck = QCheckBox("View Bugs")
  
-        buttonLayout1 = QVBoxLayout()
-        buttonLayout1.addWidget(inputlabel)
-        buttonLayout1.addWidget(self.inputline)
-        buttonLayout1.addWidget(self.parse)
-        buttonLayout1.addWidget(malletlabel)
-        buttonLayout1.addWidget(self.malletline)
-        buttonLayout1.addWidget(self.callmallet)
-        buttonLayout1.addWidget(self.enhCheck)
-        buttonLayout1.addWidget(self.bugCheck)
+        vLayout = QVBoxLayout()
+        hbox1 = QHBoxLayout()
+        hbox2 = QHBoxLayout()
+        
+        vLayout.addWidget(inputlabel)
+        hbox1.addWidget(self.inputline)
+        hbox1.addWidget(self.inputSearch)
+        vLayout.addLayout(hbox1)
+        vLayout.addWidget(self.parse)
+        
+        vLayout.addWidget(malletlabel)
+        hbox2.addWidget(self.malletline)
+        hbox2.addWidget(self.malletSearch)
+        vLayout.addLayout(hbox2)
+        vLayout.addWidget(self.callmallet)
+        vLayout.addWidget(self.enhCheck)
+        vLayout.addWidget(self.bugCheck)
  
         self.callmallet.clicked.connect(self.callMalletButton)
         self.parse.clicked.connect(self.callParseButton)
  
         mainLayout = QGridLayout()
         # mainLayout.addWidget(nameLabel, 0, 0)
-        mainLayout.addLayout(buttonLayout1, 0, 1)
+        mainLayout.addLayout(vLayout, 0, 1)
+        #mainLayout.addLayout(hLayout, 0, 1)
  
         self.setLayout(mainLayout)
         self.setWindowTitle("Next Top Model")
