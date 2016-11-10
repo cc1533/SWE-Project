d481cb117358a8570d80b041a02c22b0c2737aad
Chris <cc1533@msstate.edu>
2016-11-10 17:42:44 -0600

    Added a little more functionality to the GUI, changed layout slightly

--- ntm.py
+++ ntm.py
@@ -9,9 +9,17 @@
 #
 #####################################################################################################
 #  TODO:
-#       1.  Add Call Mallet button functionality
+#       1.  Add full button functionality
+#           - Call Mallet button
+#           - Check boxes
+#           - *add a new* display model button
+#           - # of topics to display widget (spin box?)
 #       2.  Test to make sure it runs and works correctly on Windows
-#       3.  Figure out how to get the graphing stuff to work?
+#           - Make sure that everything that uses paths can work on Windows and Linux
+#       3.  Figure out how to get the model stuff to be displayed?
+#           - Just open them in any image viewer?
+#           - Create image widget in the GUI and display it there?
+#       4.  Make sure all calls to external modules work
 #
 #####################################################################################################
 #  Program info:
@@ -23,8 +31,10 @@
 from subprocess import call
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
- 
+
+
 class Form(QWidget):
+
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
 
@@ -34,68 +44,81 @@ class Form(QWidget):
         self.setGeometry(x, y, x, y)
 
         # Create input file label, text line, search button and the parser button
-        inputlabel = QLabel("Input File Location:")
-        self.inputline = QLineEdit("/Path/To/Input/File.xlsx")
-        self.inputline.setReadOnly(True)
-        self.inputline.setToolTip("This is the path to the input file Mallet should process.")
+        inputLabel = QLabel("Input File Location:")
+        self.inputLine = QLineEdit("/Path/To/Input/File")
+        self.inputLine.setReadOnly(True)
+        self.inputLine.setToolTip("This is the path to the input file Mallet should process.")
         self.inputSearch = QPushButton("...")
         self.inputSearch.setToolTip("Find the file Mallet should process.")
         self.inputSearch.clicked.connect(self.fileSearch)
-        self.parse = QPushButton("Parse Input")
-        self.parse.setToolTip("Every input file must be parsed before it can be processed by Mallet")
-        self.parse.clicked.connect(self.callParseButton)
+        self.parseButton = QPushButton("Parse Input")
+        self.parseButton.setToolTip("Every input file must be parsed before it can be processed by Mallet")
+        self.parseButton.clicked.connect(self.callParseButton)
+        self.parseButton.setDisabled(True)
 
         # Create mallet label, text line, search button and call mallet button
-        malletlabel = QLabel("Mallet Location:")
-        self.malletline = QLineEdit("/Path/To/Mallet")
-        self.malletline.setReadOnly(True)
-        self.malletline.setToolTip("This is the path to where the Mallet program is located.")
+        malletLabel = QLabel("Mallet Location:")
+        self.malletLine = QLineEdit("/Path/To/Mallet/Program")
+        self.malletLine.setReadOnly(True)
+        self.malletLine.setToolTip("This is the path to where the Mallet program is located.")
         self.malletSearch = QPushButton("...")
         self.malletSearch.setToolTip("Find the Mallet program.")
         self.malletSearch.clicked.connect(self.fileSearch)
-        self.callmallet = QPushButton("Call Mallet")
-        self.callmallet.setToolTip("Mallet must be called so it can process the the input file.")
-        self.callmallet.clicked.connect(self.callMalletButton)
+        self.malletButton = QPushButton("Call Mallet")
+        self.malletButton.setToolTip("Mallet must be called so it can process the the input file.")
+        self.malletButton.clicked.connect(self.callMalletButton)
+        self.malletButton.setDisabled(True)
 
         # Create check boxes, these will be used later
         self.enhCheck = QCheckBox("View Enhancements")
+        self.enhCheck.setChecked(True)
         self.enhCheck.setToolTip("Show Enhancements?")
         self.bugCheck = QCheckBox("View Bugs")
+        self.bugCheck.setChecked(True)
         self.bugCheck.setToolTip("Show Bugs?")
 
+        # Create spin box widget to get the # of desired topics displayed in the model
+        self.numTopicBox = QSpinBox()
+        self.numTopicBox.setRange(1, 20)        # Arbitrarily picked 20, idk what the max should be
+        self.numTopicBox.setToolTip("Number of topics to be displayed (10 by default).")
+        self.numTopicBox.setValue(10)
+
         # Create layout boxes (makes things line up nicely)
-        vLayout = QVBoxLayout()
-        hbox1 = QHBoxLayout()
-        hbox2 = QHBoxLayout()
+        vBox1 = QVBoxLayout()
+        hBox1 = QHBoxLayout()
+        hBox2 = QHBoxLayout()
 
         # Add the first set of widgets to the window (input file stuff)
-        vLayout.addWidget(inputlabel)
-        hbox1.addWidget(self.inputline)
-        hbox1.addWidget(self.inputSearch)
-        vLayout.addLayout(hbox1)
-        vLayout.addWidget(self.parse)
+        vBox1.addWidget(inputLabel)
+        hBox1.addWidget(self.inputLine)
+        hBox1.addWidget(self.inputSearch)
+        vBox1.addLayout(hBox1)
+        vBox1.addWidget(self.parseButton)
 
         # Add the second set of widgets to the window (mallet stuff)
-        vLayout.addWidget(malletlabel)
-        hbox2.addWidget(self.malletline)
-        hbox2.addWidget(self.malletSearch)
-        vLayout.addLayout(hbox2)
-        vLayout.addWidget(self.callmallet)
+        vBox1.addWidget(malletLabel)
+        hBox2.addWidget(self.malletLine)
+        hBox2.addWidget(self.malletSearch)
+        vBox1.addLayout(hBox2)
+        vBox1.addWidget(self.malletButton)
 
         # Add checkboxes and other widgets to window
-        vLayout.addWidget(self.enhCheck)
-        vLayout.addWidget(self.bugCheck)
- 
+        hBox3 = QHBoxLayout()
+        hBox3.addWidget(self.enhCheck)
+        hBox3.addWidget(self.bugCheck)
+        vBox1.addLayout(hBox3)
+        vBox1.addWidget(self.numTopicBox)
+
         mainLayout = QGridLayout()
         # mainLayout.addWidget(nameLabel, 0, 0)
-        mainLayout.addLayout(vLayout, 0, 1)
+        mainLayout.addLayout(vBox1, 0, 1)
         #mainLayout.addLayout(hLayout, 0, 1)
  
         self.setLayout(mainLayout)
         self.setWindowTitle("Next Top Model")
  
     def callMalletButton(self):
-        name = self.malletline.text()
+        name = self.malletLine.text()
  
         if name == "":
             QMessageBox.information(self, "Error",
@@ -108,7 +131,7 @@ class Form(QWidget):
             return
     
     def callParseButton(self):
-        inputFile = self.inputline.text()
+        inputFile = self.inputLine.text()
  
         if inputFile == "":
             QMessageBox.information(self, "Error",
@@ -137,20 +160,23 @@ class Form(QWidget):
         inputfile = fname1[0][2:inputlen]    # strip off the first two characters ('
 
         # These print statements show exactly what the problem was and how it was progressively resolved
-        print(str(fname))
-        print(fname1[0])
-        print(inputfile)
+        #print(str(fname))
+        #print(fname1[0])
+        #print(inputfile)
 
         if "mallet" in inputfile:
-            self.malletline.setText(inputfile)
+            self.malletLine.setText(inputfile)
+            self.malletButton.setEnabled(True)
             return
         elif ".xls" in inputfile or ".xlsx" in inputfile:
-            self.inputline.setText(inputfile)
+            self.inputLine.setText(inputfile)
+            self.parseButton.setEnabled(True)
+            #parseReady = True
             return
         else:
             QMessageBox.information(self, "Error", "Please enter a valid input file")
             return
-        # self.inputline.setText(fname)
+        # self.inputLine.setText(fname)
 
     # This will be the function that displays the generated graphs
     # def showGraph(self):
