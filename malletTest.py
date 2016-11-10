957d1aaf2359c435e119cc48ec22a58c93d1a363
titus-dillon <tidillon28@gmail.com>
2016-11-10 01:43:39 -0600

    Added output unzipper & command line input
    - limited output to only output_state.gz
    - output goes to current directory
    - increased iterations to 30
    - removed unnecessary comments & variables
    - changed names to camel case for consistency across files

--- malletTest.py
+++ malletTest.py
@@ -1,47 +1,87 @@
+'''
+#####################################################################################################
+#	CSE 4214, Intro to Software Engineering, Fall 2016
+#	Lab Section 2, Group 3, Next Top Model
+#
+#####################################################################################################
+#	Contributors:
+#			Alex Palacio, Christopher Cole, Jia Zhao,
+#			Nathan Frank, Reid Montague, Titus Dillon
+#
+#####################################################################################################
+#	Program info:
+#			This program will:
+#                          - call mallet to perform topic modeling
+#                          - unzips mallet output_state file
+#
+#                       This program assumes the following folders are in the same directory
+#                          - inputdirectory
+#
+#####################################################################################################
+#!/usr/bin/python
+'''
+
+
 import os
+import gzip
 from subprocess import call
-
-#import subprocess
-
-os.environ['MALLET_HOME'] = "C:\\mallet"
+from sys import argv
 
 
-# This script assumes it will be run from inside C:\mallet-2.0.7
+# Sets MALLET_HOME environment variable
+os.environ['MALLET_HOME'] = "C:/mallet"
 
-# Set the path to your mallet package (make sure to change the mallet version number, if necessary)
-# Create directory "inputdirectory" within this mallet directory. Load the files to be analyzed in "inputdirectory"
-mallet_path = "C:\\mallet"
-input_path = mallet_path + "\\inputdirectory"
+malletPath = "C:/NextTopModel"
+inputPath = malletPath + "/inputdirectory"
 
-# Specify the number of topics you wish to obtain, and tbe number of iterations you wish to use
-# to obtain those topics. Store both values as string (keep the numbers in quotation marks, or use str() method below)
-# Caveat lector: Running 30000 iterations takes a few minutes...
-num_topics = "5"
-num_iterations = "20"
+numTopics = argv[1]
+numIterations = "30"
 
-# One could add additional parameters here (e.g. --optimize-interval could be helpful)
 
 class Mallet(object):
-    def __init__(self, input_path, mallet_path):
-        self.mallet_exec = mallet_path + "\\bin\\mallet"
-        self.input_path = mallet_path + "\\inputdirectory"
     
-    def import_dir(self):
-        text_path = self.input_path 
+    def __init__(self, malletPath):
+        
+        self.malletExec = malletPath + "/bin/mallet"
+    
+    def importDir(self):
+        
         output = "readyforinput.mallet"
-        call(self.mallet_exec + " import-dir --input " + input_path + " --keep-sequence --remove-stopwords --extra-stopwords extrastop.txt --output " + output, shell=True)
+        call(self.malletExec + " import-dir --input " + inputPath + " --keep-sequence --stoplist-file en.txt --output " + output, shell=True)
     
-    def train_topics(self):
-        input_file = mallet_path + "\\readyforinput.mallet"
-        output_dir = mallet_path + "\\topic_modelling_output\\"
-        output_doc_topics = output_dir + "\\output_doc_topics.txt"
-        output_topic_keys = output_dir + "\\output_topic_keys.txt"
-        output_state = output_dir + "\\output_state.gz"
-        command = self.mallet_exec + " train-topics --input " + input_file + " --num-topics " + num_topics + " --output-doc-topics " + output_doc_topics + " --output-topic-keys " + output_topic_keys + " --output-state " + output_state + " --num-iterations " + num_iterations
+    def trainTopics(self):
+        
+        inputFile = malletPath + "/readyforinput.mallet"
+        outputState = "output_state.gz"
+        command = self.malletExec + " train-topics --input " + inputFile + " --num-topics " + numTopics + " --output-state " + outputState + " --num-iterations " + numIterations
         call(command, shell=True)
 
-callmallet = Mallet(input_path, mallet_path)
-callmallet.import_dir()
-callmallet.train_topics()
+    def unzipOutput(self):
 
-# how to hardcode path: os.path.abspath("mydir\\myfile.txt")
+        zipped = gzip.open("output_state.gz", "r")
+        unzipped = open("output_state", "w")
+        content = zipped.readlines()
+
+        for line in content:
+            
+            while '\\n\'' in str(line):
+                line = str(line).replace('\\n\'', "")
+            while '\\n\"' in str(line):
+                line = str(line).replace('\\n\"', "")
+            while '\\r' in str(line):
+                line = str(line).replace('\\r', "")
+            while 'b\'' in str(line):
+                line = str(line).replace('b\'', "")
+            while 'b\"' in str(line):
+                line = str(line).replace('b\"', "")
+                
+            unzipped.write(str(line))
+            unzipped.write('\n')
+            
+        zipped.close()
+
+
+callmallet = Mallet(malletPath)
+callmallet.importDir()
+callmallet.trainTopics()
+callmallet.unzipOutput()
